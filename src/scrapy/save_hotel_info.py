a03e27345497b227d65fd5395fff90b4b068d0eb
venysssssssssss <shablaur7@hotmail.com>
2024-01-18 13:42:19 -0300

    Refactor save_hotel_info function to handle exceptions

--- src/scrapy/save_hotel_info.py
+++ src/scrapy/save_hotel_info.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 
-def save_hotel_info(self, hotels_list):
+def save_hotel_info(hotels_list):
+    try:
         df = pd.DataFrame(hotels_list)
         out_dir = 'data/out'
         os.makedirs(out_dir, exist_ok=True)
@@ -10,3 +11,5 @@ def save_hotel_info(self, hotels_list):
         df.to_excel(excel_file, index=False)
         df.to_csv(csv_file, index=False)
         print(f'There are: {len(hotels_list)} hotels.')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
