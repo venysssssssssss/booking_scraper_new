d0b8f0c628eddafc7de50734b2a1210f91b290af
venysssssssssss <shablaur7@hotmail.com>
2024-01-18 13:42:06 -0300

    Refactor init_driver function to use init_service
    This commit refactors the init_driver function in init_driver.py to use the init_service function from init_service.py. This change improves code organization and ensures that the service is properly initialized before creating the webdriver.

--- src/scrapy/init_driver.py
+++ src/scrapy/init_driver.py
@@ -1,2 +1,6 @@
-def init_driver(service):
+from selenium import webdriver
+from init_service import init_service
+
+def init_driver():
+    service = init_service()
     return webdriver.Edge(service=service)
