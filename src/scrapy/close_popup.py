05b3f478199ffac0542fd958861e9a9354bde6d5
venysssssssssss <shablaur7@hotmail.com>
2024-01-21 19:47:37 -0300

    Need to fix

--- src/scrapy/close_popup.py
+++ src/scrapy/close_popup.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def close_popup(driver):
     """
-    Close the popup window by clicking on the close button.
+    Close the popup window by clicking on the screen.
 
     Args:
         driver: The WebDriver instance.
@@ -15,10 +16,8 @@ def close_popup(driver):
 
     """
     try:
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//button[@aria-label="Close"]')
-            )
-        ).click()
+        wait = WebDriverWait(driver, 15)
+        click_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="b2searchresultsPage"]/div[48]/div/div/div/div[1]/div[1]/div/button')))
+        click_button.click()
     except Exception as e:
         print(f'An error occurred while closing the popup: {str(e)}')
