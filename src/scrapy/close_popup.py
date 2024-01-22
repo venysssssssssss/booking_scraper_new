4cdb90d2ccb8c3c7d5906d6cf21e01f067eba930
venysssssssssss <shablaur7@hotmail.com>
2024-01-21 23:02:38 -0300

    Need to fix closing pop up func

--- src/scrapy/close_popup.py
+++ src/scrapy/close_popup.py
@@ -1,4 +1,5 @@
 import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -17,7 +18,25 @@ def close_popup(driver):
     """
     try:
         wait = WebDriverWait(driver, 15)
-        click_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="b2searchresultsPage"]/div[48]/div/div/div/div[1]/div[1]/div/button')))
-        click_button.click()
+        element_present = wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="b2searchresultsPage"]/div[49]/div/div/div',
+                )
+            )
+        )
+        click_button = wait.until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    '//*[@id="b2searchresultsPage"]/div[48]/div/div/div/div[1]/div[1]/div/button',
+                )
+            )
+        )
+        if element_present and click_button:
+            time.sleep(2)  # Aguarda por 2 segundos antes de clicar
+            click_button.click()
+            wait.until_not(EC.presence_of_element_located((By.XPATH, '//*[@id="b2searchresultsPage"]/div[49]/div/div/div')))
     except Exception as e:
         print(f'An error occurred while closing the popup: {str(e)}')
