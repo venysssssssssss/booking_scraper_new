45517580f41258d5a0bbdbc18b3035957f714d2c
venysssssssssss <shablaur7@hotmail.com>
2024-01-22 00:17:25 -0300

    Add contact_dataframes function to transform.py

---
+++ src/etl/transform.py
@@ -0,0 +1,11 @@
+from typing import List
+
+import pandas as pd
+
+
+def contact_dataframes(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    função para transformar uma lista de dataframes em um único dataframe
+
+    """
+    return pd.concat(data_frame_list, ignore_index=True)
