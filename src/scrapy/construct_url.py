1f240526963699b1adc27a5e6ebdc839047d39f1
venysssssssssss <shablaur7@hotmail.com>
2024-01-21 23:02:45 -0300

    Update construct_url function to include additional parameters

--- src/scrapy/construct_url.py
+++ src/scrapy/construct_url.py
@@ -1,4 +1,4 @@
-def construct_url(checkin_date, checkout_date, destination):
+def construct_url(checkin_date, checkout_date, destination, adults, rooms, children, currency):
     """
     Constructs a URL for booking.com search results based on the provided check-in date, check-out date, and destination.
 
@@ -14,7 +14,7 @@ def construct_url(checkin_date, checkout_date, destination):
         Exception: If an error occurs during URL construction.
     """
     try:
-        url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkin_date}&checkout={checkout_date}&selected_currency=USD&ss={destination}&ssne={destination}&ssne_untouched={destination}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
+        url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkin_date}&checkout={checkout_date}&selected_currency={currency}&ss={destination}&ssne={destination}&ssne_untouched={destination}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults={adults}&no_rooms={rooms}&group_children={children}&sb_travel_purpose=leisure'
         return url
     except Exception as e:
         print(f'An error occurred: {e}')
