b028f22b33f663e8747dd4ecb5585c175419aa5a
venysssssssssss <shablaur7@hotmail.com>
2024-01-23 10:59:22 -0300

    Refactor plot_hotels function and add default color for NaN values

--- src/models/decision_tree_hotels/view_best_hotels.py
+++ src/models/decision_tree_hotels/view_best_hotels.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import mplcursors
 import numpy as np
 import pandas as pd
-from src.models.decision_tree_hotels.view_best_hotels import review_mapping
 
 def load_data(file_path):
     """
@@ -22,7 +21,7 @@ def preprocess_data(df):
         'Good': 1,
         'Very Good': 2,
         'Wonderful': 3,
-        'Review score': 4,
+        'Ok': 4,
         'Excellent': 5,
         'Exceptional': 6,
     }
@@ -30,44 +29,56 @@ def preprocess_data(df):
     
     df['reviews count'] = df['reviews count'].str.replace(',', '').astype(float)
     
-    return df
+    return df, review_mapping
 
-def plot_hotels(df):
+def plot_hotels(df, review_mapping):
     """
     Plot hotels based on price and reviews count, with different colors for each review category.
     """
     colors = plt.cm.rainbow(np.linspace(0, 1, len(df['avg review'].unique())))
     color_dict = dict(zip(df['avg review'].unique(), colors))
 
+    # Add a default color for NaN values
+    default_color = [0, 0, 0, 1]  # Black color for NaN values
+
+    fig, ax = plt.subplots()
+
     for review in df['avg review'].unique():
         review_data = df[df['avg review'] == review]
-        plt.scatter(
+        if pd.isnull(review):
+            color = default_color  # Use a default color if review is NaN
+            label = 'NaN'  # Use 'NaN' as label if review is NaN
+        else:
+            color = color_dict[review]
+            label = list(review_mapping.keys())[list(review_mapping.values()).index(review)]
+        scatter = ax.scatter(
             review_data['price'],
             review_data['reviews count'],
-            c=[color_dict[review]],
-            label=list(review_mapping.keys())[
-                list(review_mapping.values()).index(review)
-            ],
+            c=[color],
+            label=label,
         )
 
-    plt.xlabel('Price')
+    plt.xlabel('Price (diária)')
     plt.ylabel('Reviews Count')
     plt.title('Hotels')
     plt.legend()
-    
-    mplcursors.cursor(hover=True)
-    
+
+    cursor = mplcursors.cursor(ax, hover=True)
+    cursor.connect("add", lambda sel: sel.annotation.set_text(df["hotel"].iloc[sel.target.index]))
+
     plt.show()
+
+
 
 def main():
     # Load the data
     df = load_data('data/out/output_etl_data/all_hotels_pages_in_1_file.xlsx')
 
     # Preprocess the data
-    df = preprocess_data(df)
+    df, review_mapping = preprocess_data(df)
 
     # Plot the hotels
-    plot_hotels(df)
+    plot_hotels(df, review_mapping)
 
 if __name__ == "__main__":
     main()
